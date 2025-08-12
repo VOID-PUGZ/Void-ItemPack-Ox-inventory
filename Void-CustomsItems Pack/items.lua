d9094e5c625b7b1fa35ada612533af06051e2a4b
VOID-PUGZ <scottyh330@gmail.com>
2025-08-12 14:35:52 +0100

    Add files via upload

---
+++ Void-CustomsItems Pack/items.lua
@@ -0,0 +1,231 @@
+["weed_dryer"] = {
+    label = "Weed Dryer",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["weed_packed_10g"] = {
+    label = "Weed 10G",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["weed_pot"] = {
+    label = "Plant Pot",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["weed_seed_1"] = {
+    label = "Weed Seed",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["weedbag"] = {
+    label = "2G Cali",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["weedscissors"] = {
+    label = "Weedscissors",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["weed_butter"] = {
+    label = "Weed Butter",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["weed_oil"] = {
+    label = "Weed Oil",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["weed_gummies"] = {
+    label = "Weed Gummies",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["coke_leafpile"] = {
+    label = "Coke Leafpile",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["leafblower"] = {
+    label = "Leafblower",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["coke_shear"] = {
+    label = "Shears",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["coke_pile"] = {
+    label = "Coke Pile",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["cutted_coke"] = {
+    label = "Cutted Coke",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["spoon"] = {
+    label = "Spoon",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["fan_2"] = {
+    label = "Drying Fan",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["meth_crystal"] = {
+    label = "Meth Crystal",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["meth_beaker"] = {
+    label = "Meth Beaker",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["mixture"] = {
+    label = "Mixture",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["meth_packed"] = {
+    label = "Meth Packed",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["meth_tray"] = {
+    label = "Meth Tray",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["plant_spray"] = {
+    label = "Plant Spray",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["Big Diamond"] = {
+    label = "Big Diamond",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["bolt_cutter"] = {
+    label = "Bolt Cutter",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["diamond"] = {
+    label = "Diamond",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["keycarda"] = {
+    label = "Black KeyCard",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["green_tube"] = {
+    label = "Green Tube",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["cashroll"] = {
+    label = "CashRoll",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["babybw"] = {
+    label = "Baby Bottle",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["movie_ticket"] = {
+    label = "Movie Ticket",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["mystery_box"] = {
+    label = "Mystery Box!",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["rope"] = {
+    label = "Thick Rope",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["house_keys"] = {
+    label = "House Keys",
+    weight = 1,
+    stack = true,
+    close = true
+},
+["harness_purple"] = {
+    label = "Purple RaceHarness",
+    weight = 1,
+    stack = true,
+    close = true
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
